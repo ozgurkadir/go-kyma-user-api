2c728695d6d7c99495aca5f26dacf2c76e9607ac
ozgurkadir <kadirozgur@outlook.com>
2022-07-19 23:21:20 +0200

    project structure

---
+++ pkg/user/user.go
@@ -0,0 +1,8 @@
+type User struct {
+	UserName  string `json:"username`
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Address	  string `json:"address"`
+	Mobile    int    `json:"mobile"`
+}
