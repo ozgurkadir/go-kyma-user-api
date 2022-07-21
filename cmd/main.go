c99c64195ca294dd410fef7b13c9668af25499bc
ozgurkadir <kadirozgur@outlook.com>
2022-07-21 22:56:24 +0200

    Db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"log"
+	"database/sql"
 )
 
 func main() {
 
+	dbConnectionError := connectToHanaDB()
+	if dbConnectionError != nil {
+		log.Fatalln(dbConnectionError)
+		return
+	}
+ 
+}
+
+
+func connectToHanaDB() (error){
+	user := os.Getenv("HDB_USER")
+	password := os.Getenv("HDB_PASSWORD")
+	host := os.Getenv("HDB_HOST")
+	port := os.Getenv("HDB_PORT")
+	pemFile := "DigiCertGlobalRootCA.pem"
+	connectionString := "hdb://" + user + ":" + password + "@" + host + ":" + port + "?TLSServerName=" + host + "&TLSRootCAFile=" + pemFile
+
+	db, err := sql.Open("hdb", connectionString)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+
+    pingErr := db.Ping()
+    if pingErr != nil {
+        log.Fatal(pingErr)
+		return err
+    }
+    //fmt.Println("Connected!")
+	return nil
 }
